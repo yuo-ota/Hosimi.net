581334636066069bf2bfa1afa518da01164ef361
yuo_ota <yuuta.tsunashima@gmail.com>
2025-08-07 09:43:00 +0900

    fix: TypekitLoader.ts's path changed from @/type to @/utils

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import TypekitLoader from "@/type/TypekitLoader";
+import TypekitLoader from "@/utils/TypekitLoader";
 
 export const metadata: Metadata = {
   title: "Astronom",
