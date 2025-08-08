4ae8ff87652f9d0eb4306676f8a127e3a079732d
yuo_ota <yuuta.tsunashima@gmail.com>
2025-08-08 15:29:47 +0900

    style: delete 'text-font' in className

--- frontend/src/components/Headline.tsx
+++ frontend/src/components/Headline.tsx
@@ -10,9 +10,7 @@ const Headline = ({ title, description }: HeadlineProps) => {
         <div className="w-[3px] bg-accent" />
         <div className="flex flex-col gap-y-1 py-[2px]">
           <span className="font-title text-foreground text-6xl">{title}</span>
-          <span className="font-body text-foreground text-2xl">
-            {description}
-          </span>
+          <span className="text-foreground text-2xl">{description}</span>
         </div>
       </div>
     </>
