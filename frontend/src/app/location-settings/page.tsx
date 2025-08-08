c93e27bacb988af668c5ac978f8525c649b799c3
yuo_ota <yuuta.tsunashima@gmail.com>
2025-08-08 16:47:28 +0900

    add: location-settings page draft

--- frontend/src/app/location-settings/page.tsx
+++ frontend/src/app/location-settings/page.tsx
@@ -1,3 +1,18 @@
+import Headline from "@/components/Headline";
+import LocationSetting from "@/features/LocationSetting/LocationSetting";
+
 export default function LocationSettingPage() {
-  return <></>;
+  return (
+    <>
+      <div className="w-dvw h-dvh flex justify-center">
+        <div className="w-17/20">
+          <Headline
+            title="観測地点設定方法選択"
+            description="観測値を設定するための方法を選んでください"
+          />
+          <LocationSetting />
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ frontend/src/features/LocationSetting/LocationSetting.tsx
@@ -0,0 +1,5 @@
+const LocationSetting = () => {
+  return <></>;
+};
+
+export default LocationSetting;
