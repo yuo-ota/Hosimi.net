223f8546612bdec44712335e317b38d9884158ae
yuo_ota <yuuta.tsunashima@gmail.com>
2025-08-08 21:42:44 +0900

    add: LocationSettingButton component

---
+++ frontend/src/components/LocationSettingButton.tsx
@@ -0,0 +1,41 @@
+import { ReactNode } from "react";
+
+type LocationSettingButtonProps = {
+  icon: SvgIconData;
+  buttonTitle: string;
+  buttonDescription: ReactNode;
+  handleClick: () => void;
+  className?: string;
+};
+
+const LocationSettingButton = ({
+  icon,
+  buttonTitle,
+  buttonDescription,
+  handleClick,
+  className = "",
+}: LocationSettingButtonProps) => {
+  return (
+    <>
+      <button
+        className={`${className} flex flex-col items-center justify-center gap-y-10 bg-foreground/30 backdrop-blur-lg
+    rounded-md border border-foreground/30 shadow-lg`}
+        onClick={handleClick}
+      >
+        <div className="w-full h-2/5 flex justify-center items-center">
+          <img
+            src={`${icon.path}`}
+            alt={`${icon.alt}`}
+            className="h-full w-auto"
+          />
+        </div>
+        <div className="flex flex-col items-center justify-around">
+          <span className="font-title text-5xl">{buttonTitle}</span>
+          {buttonDescription}
+        </div>
+      </button>
+    </>
+  );
+};
+
+export default LocationSettingButton;
---
+++ frontend/src/type/SvgIconData.ts
@@ -0,0 +1,4 @@
+type SvgIconData = {
+  path: string;
+  alt: string;
+};
