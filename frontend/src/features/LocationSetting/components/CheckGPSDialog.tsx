836e1963311764f4a37be249be29bce282205366
yuo_ota <yuuta.tsunashima@gmail.com>
2025-08-08 23:57:54 +0900

    feat: GPS check dialog responsive

--- frontend/src/features/LocationSetting/components/CheckGPSDialog.tsx
+++ frontend/src/features/LocationSetting/components/CheckGPSDialog.tsx
@@ -8,7 +8,7 @@ const CheckGPSDialog = () => {
   return (
     <>
       <div
-        className="top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-2/5 h-[80dvh] p-5 flex flex-col items-start absolute z-20 bg-foreground/30 backdrop-blur-lg
+        className="top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/2 h-[80dvh] p-5 flex flex-col items-start absolute z-20 bg-foreground/30 backdrop-blur-lg
     rounded-md border border-foreground/30 shadow-lg"
       >
         <div className="w-full flex justify-between items-start justify-items-end">
@@ -16,7 +16,7 @@ const CheckGPSDialog = () => {
             preferSmall={true}
             title="観測地点確認"
             description="指定された地点はこちらで間違いありませんか？"
-            className="mb-10 lg:mb-20"
+            className="mb-5 lg:mb-10"
           />
           <img
             src={`${CloseIcon.src}`}
@@ -24,6 +24,7 @@ const CheckGPSDialog = () => {
             className="aspect-square h-full max-h-[60px]"
           />
         </div>
+        <div className="flex-1 w-full bg-red-200 mb-5 lg:mb-10"></div>
         <div className="flex mt-auto w-full">
           <DualButton
             buttons={[
@@ -37,16 +38,16 @@ const CheckGPSDialog = () => {
                   </>
                 ),
                 handleClick: () => console.log("戻るボタン"),
-                className: "flex-1 px-4 py-2",
+                className: "flex-1 w-full lg:px-4 py-2",
               },
               {
                 isPriority: false,
                 children: "確認",
                 handleClick: () => console.log("確認ボタン"),
-                className: "flex-1 px-4 py-2 text-base md:text-xl",
+                className: "flex-1 w-full lg:px-4 py-2 text-base md:text-xl",
               },
             ]}
-            className="flex w-full justify-between items-center h-[45px] md:h-[77px] gap-5"
+            className="flex w-full justify-between items-center gap-x-5 gap-y-3 flex-col lg:flex-row"
           />
         </div>
       </div>
